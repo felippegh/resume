3e6640de61a7158e743554492ef6e68480572d0a
Felippe <felippegeorge@utexas.edu>
2022-06-09 20:48:00 -0300

    Updates

---
+++ precache-manifest.4064f98dd387828ded9cfc072942cdc1.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2458bd77291f7004738ed5cc5d2a8589",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a1c3bc2d1a719788d9",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e7c3beff7dfb6f575f83",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "28f40c7f785c12740140",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "76570c2cb36722ea794e",
+    "url": "/static/js/2.7c0471e4.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.7c0471e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a1c3bc2d1a719788d9",
+    "url": "/static/js/3.ac9a53b7.chunk.js"
+  },
+  {
+    "revision": "e7c3beff7dfb6f575f83",
+    "url": "/static/js/4.1ff83a36.chunk.js"
+  },
+  {
+    "revision": "28f40c7f785c12740140",
+    "url": "/static/js/main.9162c4ab.chunk.js"
+  },
+  {
+    "revision": "c6fcaf65708622e1bd7d",
+    "url": "/static/js/runtime-main.876c440d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f6402eb3eb96f636980389d25e263dd3",
+    "url": "/static/media/atos.f6402eb3.jpg"
+  },
+  {
+    "revision": "fff3b0df3a68c6d5d28c4523695f1322",
+    "url": "/static/media/atos.fff3b0df.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "914d36b68b71a7ef68d9455e8b5638d0",
+    "url": "/static/media/csun.914d36b6.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "cc2a6d7415c9349467d01046f98f560d",
+    "url": "/static/media/ecp.cc2a6d74.png"
+  },
+  {
+    "revision": "f0d06059e77dbffd3c3f96c0bc1e51a9",
+    "url": "/static/media/ecp2.f0d06059.png"
+  },
+  {
+    "revision": "084be0871232cf222178070fba2a884a",
+    "url": "/static/media/embraco.084be087.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "b669382bf95adffd200916ea8ae87a2b",
+    "url": "/static/media/icesbg.b669382b.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "696605e132cf8b20acd6a0e6b367f80d",
+    "url": "/static/media/sanofi.696605e1.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "58b9eba85b7fc38f73e421cd6f3ee2b8",
+    "url": "/static/media/swarovski.58b9eba8.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "748417890f866918f0a820c056e1a94d",
+    "url": "/static/media/utexas.74841789.png"
+  },
+  {
+    "revision": "49aa3fa42d9fd90bdac95b76911af11b",
+    "url": "/static/media/utfpr.49aa3fa4.jpg"
+  }
+]);
